9bd592e945219eaf4c29241d27299360af147b99
Mehmet Akif Albayrak <akifalbayrakoffical@gmail.com>
2024-11-13 12:45:31 +0300

    Feat:

--- EventGoAPI/Core/EventGoAPI.Application/Features/Query/User/LoginUser/LoginUserQueryHandler.cs
+++ EventGoAPI/Core/EventGoAPI.Application/Features/Query/User/LoginUser/LoginUserQueryHandler.cs
@@ -1,7 +1,9 @@
 ﻿using EventGoAPI.Application.Abstractions.Repositories;
 using EventGoAPI.Application.Abstractions.Services;
 using EventGoAPI.Application.Enums;
+using EventGoAPI.Application.Features.Command.Participant.CreateParticipant;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,14 +17,25 @@ namespace EventGoAPI.Application.Features.Query.User.LoginUser
         private IUserReadRepository _userReadRepository;
         private IPasswordHasher _passwordHasher;
         private ITokenService _tokenService;
-        public LoginUserQueryHandler(IUserReadRepository userReadRepository, ITokenService tokenService, IPasswordHasher passwordHasher)
+        private IHttpContextAccessor _httpContextAccessor;
+        public LoginUserQueryHandler(IUserReadRepository userReadRepository, ITokenService tokenService, IPasswordHasher passwordHasher, IHttpContextAccessor httpContextAccessor)
         {
             _userReadRepository = userReadRepository;
             _tokenService = tokenService;
             _passwordHasher = passwordHasher;
+            _httpContextAccessor = httpContextAccessor;
         }
         public async Task<LoginUserQueryResponse> Handle(LoginUserQueryRequest request, CancellationToken cancellationToken)
         {
+            if (_httpContextAccessor.HttpContext?.Items["UserId"] is Guid userId)
+            {
+                return new LoginUserQueryResponse
+                {
+                    Success = false,
+                    Message = "You Are Already Logged.",
+                    ResponseType = ResponseType.Unauthorized
+                };
+            }
             var user = await _userReadRepository.GetUserByUsernameAsync(request.Username);
 
             if (user == null || !_passwordHasher.VerifyPassword(request.Password, user.PasswordHash))

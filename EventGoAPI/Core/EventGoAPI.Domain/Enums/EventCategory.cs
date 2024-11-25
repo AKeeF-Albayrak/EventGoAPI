5dc9c1cafd1a5c6a7ce04ca7989e28babce9772f
Mehmet Akif Albayrak <akifalbayrakoffical@gmail.com>
2024-11-26 00:14:42 +0300

    Feat:

--- EventGoAPI/Core/EventGoAPI.Domain/Enums/EventCategory.cs
+++ EventGoAPI/Core/EventGoAPI.Domain/Enums/EventCategory.cs
@@ -8,8 +8,20 @@ namespace EventGoAPI.Domain.Enums
 {
     public enum EventCategory
     {
-        Sport = 0,
-        Music = 1,
-
+        Spor = 0,
+        Müzik = 1,
+        Sanat = 2,
+        Teknoloji = 3,
+        Bilim = 4,
+        Edebiyat = 5,
+        Sinema = 6,
+        Tiyatro = 7,
+        Fotoğrafçılık = 8,
+        Seyahat = 9,
+        Yemek = 10,
+        Dans = 11,
+        Yoga = 12,
+        Doğa = 13,
+        Tarih = 14,
     }
 }
